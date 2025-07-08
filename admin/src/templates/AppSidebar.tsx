80861be55dd71b0505632c0e15212b15ff7769f5
hated <emad.amoupour1997@gmail.com>
2025-07-08 07:07:01 +0330

    sidebar ui create

--- admin/src/templates/AppSidebar.tsx
+++ admin/src/templates/AppSidebar.tsx
@@ -1,48 +1,95 @@
 import React from "react";
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import {
+  Tags,
+  Home,
+  UserRoundSearch,
+  Search,
+  Settings,
+  User,
+  Pencil,
+  PencilLine,
+  PencilOff,
+  UserRoundPlus,
+  UserRoundCog,
+  LogOut,
+} from "lucide-react";
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 // Menu items.
 const items = [
   {
     title: "Home",
-    url: "#",
+    url: "/",
     icon: Home,
   },
   {
-    title: "Inbox",
+    title: "Users Table",
     url: "#",
-    icon: Inbox,
+    icon: UserRoundSearch,
   },
   {
-    title: "Calendar",
+    title: "Ticket Table",
     url: "#",
-    icon: Calendar,
+    icon: Tags,
+  },
+];
+const tickets = [
+  {
+    title: "Create Ticket",
+    url: "/CreateTibkrt",
+    icon: Pencil,
   },
   {
-    title: "Search",
+    title: "Edit Ticket",
     url: "#",
-    icon: Search,
+    icon: PencilLine,
+  },
+];
+
+const admins = [
+  {
+    title: "Create Admin",
+    url: "#",
+    icon: UserRoundPlus,
   },
   {
-    title: "Settings",
+    title: "Edit admin",
     url: "#",
-    icon: Settings,
+    icon: UserRoundCog,
   },
 ];
 
 function AppSidebar() {
   return (
-    <Sidebar>
+    <Sidebar collapsible="icon">
+      <SidebarHeader className="h-20 bg-slate-800 text-white w-full">
+        <SidebarMenu className="flex items-center h-full w-full  ">
+          <SidebarMenuItem className="flex items-center h-full w-full justify-between">
+            <SidebarMenuButton className="">
+              <Avatar className="">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  className="size-12 object-cover"
+                />
+                <AvatarFallback className="">username</AvatarFallback>
+              </Avatar>
+              <User />
+              <span className="text-lg text-white">username</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
       <SidebarContent className="bg-slate-800 text-white ">
         <SidebarGroup>
           <SidebarGroupLabel className="text-white">
@@ -63,7 +110,51 @@ function AppSidebar() {
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-white">Ticket</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {tickets.map((ticket) => (
+                <SidebarMenuItem key={ticket.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={ticket.url}>
+                      <ticket.icon />
+                      <span>{ticket.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-white">Ticket</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {admins.map((admin) => (
+                <SidebarMenuItem key={admin.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={admin.url}>
+                      <admin.icon />
+                      <span>{admin.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter className="bg-slate-900 text-white">
+        <SidebarMenu className="flex-row-reverse">
+          <SidebarMenuItem>
+            <SidebarMenuButton>
+              <span>Log out</span>
+              <LogOut />
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 }
